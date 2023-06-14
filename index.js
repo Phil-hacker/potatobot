1fc2912e7bb5c859098c7df80757025549fe7c1c
Phil-hacker <61991187+Phil-hacker@users.noreply.github.com>
2023-06-14 11:27:35 +0200

    added eslint and fixed all errors that occured (#1)

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits } = require("discord.js");
 const token = process.env.potatotoken;
-const logger = require('./logger');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-
-// Commands setup
-client.commands = new Collection();
-
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.warn();(`The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
-}
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
