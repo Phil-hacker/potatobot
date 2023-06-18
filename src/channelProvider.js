d1b781abd11c700d4fe4bd96b08b83868b5cd481
Phil-hacker <61991187+Phil-hacker@users.noreply.github.com>
2023-06-18 13:26:33 +0200

    added channelProvider.js (#6)

---
+++ src/channelProvider.js
@@ -0,0 +1,10 @@
+const channelProvider = {
+    devChannels: {},
+    prodChannels: {},
+    getChannel: function (channel) {
+        return process.env.mode==='DEV' ? this.devChannels[channel] : this.prodChannels[channel]
+    }
+}
+
+
+export channelProvider;
